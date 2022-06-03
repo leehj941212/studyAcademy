3b9c0ab64eee4099ac7ff326567471a43ddbf281
leehj941212@gmail.com <leehj941212@gmail.com>
2022-06-03 09:43:14 +0900

    220603

---
+++ 220602/src/Study9.java
@@ -0,0 +1,39 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+
+//사이즈가 6 ArrayList 선언
+//1~45 사이의 랜덤값 저장 -> 중복되지 않아야함.
+//정렬해서 출력
+
+//class Study10{
+//	Integer numChack(int y) {
+//		//사이즈가 6 ArrayList 선언 (6 선언은 의미없음)
+//				ArrayList<Integer> arrayList = new ArrayList<Integer>(6);
+//				
+//				//arrayList.size() 가 6이 될때까지 반복
+//				for (; arrayList.size() < 6;) {
+//					Integer x = (int)(Math.random()*45+1);
+//						if (!arrayList.contains(x)) {
+//						arrayList.add(x);
+//						}	
+//				}
+//				
+//				//작은 순으로 정렬
+//				Collections.sort(arrayList);
+//				
+//				//System.out.println(arrayList);
+//				for (Integer i : arrayList) {
+//					System.out.println(i);
+//					
+//				return arrayList.get(y);
+//				
+//				
+//				}
+//	}
+
+	
+public class Study9 {
+	
+	public static void main(String[] args) {
+		}
+	}
